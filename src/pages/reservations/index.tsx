40847f86b0a9be7dd26700d6eda9ec7289377c81
roq-ai <savin171099@gmail.com>
2023-05-26 06:06:47 +0000

    Initial commit

---
+++ src/pages/reservations/index.tsx
@@ -0,0 +1,59 @@
+import AppLayout from 'layout/app-layout';
+import { Table, Thead, Tbody, Tr, Th, Td, TableContainer, Box, Text } from '@chakra-ui/react';
+import useSWR from 'swr';
+import { Spinner } from '@chakra-ui/react';
+import { getReservations } from 'apiSdk/reservations';
+import { ReservationInterface } from 'interfaces/reservation';
+import { Error } from 'components/error';
+
+function ReservationListPage() {
+  const { data, error, isLoading } = useSWR<ReservationInterface[]>(
+    () => '/reservations',
+    () =>
+      getReservations({
+        relations: ['user', 'restaurant'],
+      }),
+  );
+
+  return (
+    <AppLayout>
+      <Text as="h1" fontSize="2xl" fontWeight="bold">
+        Reservation
+      </Text>
+      <Box bg="white" p={4} rounded="md" shadow="md">
+        {error && <Error error={error} />}
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <TableContainer>
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                  <Th>Reservation ID</Th>
+                  <Th>Date</Th>
+                  <Th>Time</Th>
+                  <Th>Party Size</Th>
+                  <Th>Customer</Th>
+                  <Th>Restaurant</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data?.map((record) => (
+                  <Tr key={record.id}>
+                    <Td>{record.id}</Td>
+                    <Td>{record.date as unknown as string}</Td>
+                    <Td>{record.time as unknown as string}</Td>
+                    <Td>{record.party_size}</Td>
+                    <Td>{record.user?.roq_user_id}</Td>
+                    <Td>{record.restaurant?.name}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        )}
+      </Box>
+    </AppLayout>
+  );
+}
+export default ReservationListPage;
